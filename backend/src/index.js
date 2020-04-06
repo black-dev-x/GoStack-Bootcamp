a82269abee440facfac41cc833d7e176c2848fae
thiagodevx <thiago.dev.x@gmail.com>
2020-04-06 18:18:44 -0300

    métodos

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express()
 
-const projects = ['projeto galinha 2.0']
-app.get('/projects', (req, res) => {
-  res.json(projects)
+const projetos = ['projeto 1', 'projeto 2', 'projeto 3']
+
+app.get('/projetos', (requisicao, resposta) => {
+  resposta.json(projetos)
 })
+
+app.post('/projetos', (requisicao, resposta) => {
+  resposta.json(projetos)
+})
+
+app.put('/projetos/:id', (requisicao, resposta) => {
+  resposta.json(projetos)
+})
+
+app.delete('/projetos/:id', (requisicao, resposta) => {
+  resposta.json(projetos)
+})
+
 app.listen(3333, () => console.log('(╯°□°）╯︵ ┻━┻ Servidor esta rodando'))
+//Sabia que ao utilizar a tecla do Windows + Ponto, você abre aqui no visual studio
+//um menu para adicionar emotes? 🐱‍👤
