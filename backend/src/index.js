a9be86059111071534a9f0e83971587fb9f07a06
thiagodevx <thiago.dev.x@gmail.com>
2020-04-07 23:00:15 -0300

    updates no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { uuid } = require('uuidv4')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 let projects = []
