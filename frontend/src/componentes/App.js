fe9f9b3354cf1387c6f82fab3fe40860e1208b64
thiagodevx <thiago.dev.x@gmail.com>
2020-04-07 22:01:30 -0300

    componente criado

---
+++ frontend/src/componentes/App.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default _ => <h1>Componente App</h1>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './componentes/App'
 
-render(<h1>Hello World</h1>, document.querySelector('#app'))
+render(<App />, document.querySelector('#app'))
