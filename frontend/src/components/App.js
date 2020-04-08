20b103d3ce6d3399bba5a5ca284f35ebbba52cca
thiagodevx <thiago.dev.x@gmail.com>
2020-04-07 22:58:11 -0300

    fazendo requisição, e obtendo um erro de CORS

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Header'
+import api from '../services/api'
 import './App.css'
-import backgroundImage from '../assets/imagem.jpg'
 
 export default _ => {
   const [projects, setProjects] = useState([
     'Desenvolvimento de app',
     'Front-end web'
   ])
-
+  useEffect(() => {
+    api.get('projects').then(response => setProjects(response.data))
+  }, [])
   const addProject = () => {
     setProjects([...projects, 'Novo projeto'])
   }
   return (
     <>
       <Header title="ReactJS" />
-      <img src={backgroundImage}></img>
       <ul>
         {projects.map((project, index) => (
           <li key={index}>{project}</li>
