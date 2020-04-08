ed50a3ffce9a576f2a66b7337db2c36e7120833b
thiagodevx <thiago.dev.x@gmail.com>
2020-04-08 14:53:04 -0300

    inicio do projeto

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
 
-export default _ => <View />
+export default _ => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1"></StatusBar>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello world</Text>
+    </View>
+  </>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+})
